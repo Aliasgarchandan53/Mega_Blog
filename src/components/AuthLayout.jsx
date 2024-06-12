839d79742eaf76abd5fa9cb0b05d138a536679bf
Aliasgarchandan53 <aliasgarchandan53@gmail.com>
2024-06-13 00:01:54 +0530

    authlayout added

---
+++ src/components/AuthLayout.jsx
@@ -0,0 +1,29 @@
+import React ,{useState,useEffect}from 'react'
+import {useSelector} from "react-redux"
+import {useNavigate} from "react-router-domz"
+
+
+export default function Protected({
+    children,
+    authentication=true
+}) {
+
+    const navigate=useNavigate()
+    const [loader,setLoader]=useState(true)
+    const authStatus = useSelector(state=>state.auth.status)
+
+    useEffect(()=>{
+         //TODO: make it more easy to understand
+         //let authValue  = authStatus===true?true:false;
+        if(authentication && authStatus!==authentication ){
+            navigate('/login')
+        }else if(!authentication && authStatus!==authentication){
+            navigate("/")
+        }
+        setLoader(false)
+    },[authStatus,navigate,authentication])
+    
+    return loader ? <h1>Loading...</h1> : <>{children}</>
+}
+
+
