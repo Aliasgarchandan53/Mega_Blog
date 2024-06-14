0867ff2d055de948e57bd541210764cbade6e316
Aliasgarchandan53 <aliasgarchandan53@gmail.com>
2024-06-14 11:03:11 +0530

    configured routes

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,101 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from "./store/store.js"
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Home from './pages/Home.jsx'
+import { AuthLayout, Login } from './components/index.js'
+
+
+import AddPost from "./pages/AddPost";
+import Signup from './pages/Signup'
+import EditPost from "./pages/EditPost";
+
+import Post from "./pages/Post";
+
+import AllPosts from "./pages/AllPosts";
+
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+//     children: [
+//         {
+//             path: "/",
+//             element: <Home />,
+//         },
+//         {
+//             path: "/login",
+//             element: (
+//                 <AuthLayout authentication={false}>
+//                     <Login />
+//                 </AuthLayout>
+//             ),
+//         },
+//         {
+//             path: "/signup",
+//             element: (
+//                 <AuthLayout authentication={false}>
+//                     <Signup />
+//                 </AuthLayout>
+//             ),
+//         },
+//         {
+//             path: "/all-posts",
+//             element: (
+//                 <AuthLayout authentication>
+//                     {" "}
+//                     <AllPosts />
+//                 </AuthLayout>
+//             ),
+//         },
+//         {
+//             path: "/add-post",
+//             element: (
+//                 <AuthLayout authentication>
+//                     {" "}
+//                     <AddPost />
+//                 </AuthLayout>
+//             ),
+//         },
+//         {
+//             path: "/edit-post/:slug",
+//             element: (
+//                 <AuthLayout authentication>
+//                     {" "}
+//                     <EditPost />
+//                 </AuthLayout>
+//             ),
+//         },
+//         {
+//             path: "/post/:slug",
+//             element: <Post />,
+//         },
+//     ],
+// },
+// ])
+
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path='/' element={<App/>} >
+    <Route path='/' element={<Home />}/>
+    <Route path='/login' element={<AuthLayout authentication={false}><Login/></AuthLayout>}/>
+    <Route path='/signup' element={<AuthLayout authentication={false}><Signup/></AuthLayout>}/>
+    <Route path='/all-posts' element={<AuthLayout authentication><AllPosts/></AuthLayout>}/>
+    <Route path='/add-post' element={<AuthLayout authentication><AddPost/></AuthLayout>}/>
+    <Route path='/edit-post/:slug' element={<AuthLayout authentication><EditPost/></AuthLayout>}/>
+    <Route path='/post/:slug' element={<Post/>}/>
+  </Route>
+))
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+    <RouterProvider router={router}/>
     </Provider>
   </React.StrictMode>,
 )
